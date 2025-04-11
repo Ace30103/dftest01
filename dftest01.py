56bfe49a4c791d1e11a1e9fd81a7f006af7b42e2
Ace30103 <164269184+Ace30103@users.noreply.github.com>
2025-04-11 18:24:39 -0500

    Add files via upload

---
+++ dftest01.py
@@ -0,0 +1,16 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print("request made")
+        self.send_response(200) #200, OK
+        self.send_header('content-type','application/json') #return type is a json / dictionary
+        self.end_headers()
+        if self.path == "/Type1":
+            self.wfile.write('{"out":1}'.encode())
+        else:
+            self.wfile.write('{"out":0}'.encode())
+         #the data that DF gets
+
+server = HTTPServer(('localhost',8080),MyHandler)
+server.serve_forever()
